8c721f637c115caa5cc43564b614f482af4b26b0
Hazem Ali <clinysmar@gmail.com>
2025-10-27 14:40:58 +0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,18 @@
+import { nextCookies } from 'better-auth/next-js';
+import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@smart-apps/db";
+import * as schema from "@smart-apps/db/schema/auth";
+
+export const auth = betterAuth<BetterAuthOptions>({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+
+		schema: schema,
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+  plugins: [nextCookies()]
+});
